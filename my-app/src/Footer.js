bcac7a619f1590d0127c7f3bca07b79d5a8ef954
navycui <haijun82923@me.com>
2020-01-01 17:22:32 +0900

    react app module add
    react app module add

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -9,6 +9,22 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 class StateExample extends React.Component {
 
+    state = {
+        posts: []
+    };
+
+    async componentDidMount() {
+        try {
+            const res = await fetch('http://127.0.0.1:8000/api/');
+            const posts = await res.json();
+            this.setState({
+                posts
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
 render(){
     
     return (
@@ -206,6 +222,14 @@ render(){
                     </Container>
                 </Jumbotron>
                 <Container>
+                <div>
+                {this.state.posts.map(item => (
+                    <div key={item.id}>
+                        <h1>{item.title}</h1>
+                        <span>{item.content}</span>
+                    </div>
+                ))}
+                </div>
         </Container>
         </section>
 
