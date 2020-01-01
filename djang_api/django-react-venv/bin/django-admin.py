e9c0be49059bbf5706431799358ac52105191777
navycui <haijun82923@me.com>
2020-01-01 16:48:26 +0900

    django api add
    django 추가 작업 :+1:
       restframework 추가
      corsheaders 추가

---
+++ djang_api/django-react-venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/haegun/Documents/GitHub/react_bootstrap_project/djang_api/django-react-venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
